03c620b6d5c6cd441ca71af7f989d1a292d7bcdc
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-02-21 13:49:36 +0700

    add spring form login using jdbcauthentication

---
+++ authorities.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `ix_auth_username` (`username`,`authority`),
+  CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
